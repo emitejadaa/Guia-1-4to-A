33d7c736626cf556407b1903b36b65035b41f742
emitejadaa <66174235+emitejadaa@users.noreply.github.com>
2025-04-08 11:41:07 -0300

    Conversion de Monedas

---
+++ Assets/Scripts/ConversionDeMoneda.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversionDeMoneda : MonoBehaviour
+{
+    public float montoAConvertir;
+    public string codigoMoneda;
+    float cotizacionMoneda;
+    float montoConvertido;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // paso 1
+        if (montoAConvertir < 1000)
+        {
+            Debug.Log("el monto minimo es 1000");
+            return;
+        }
+        if (codigoMoneda == "D")
+        {
+            cotizacionMoneda = 1204.35f;
+        }else if (codigoMoneda == "E")
+        {
+            cotizacionMoneda = 1541.11f;
+        }else if (codigoMoneda == "R")
+        {
+            cotizacionMoneda = 201.24f;
+        }
+        else
+        {
+            Debug.Log("el codigo de moneda es invalido");
+            return;
+        }
+        // paso 2
+        montoConvertido = montoAConvertir / cotizacionMoneda;
+        // paso 3
+        Debug.Log("monto convertido: "+montoConvertido);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
